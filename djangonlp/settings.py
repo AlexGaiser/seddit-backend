f7838be3a235df80356f8deff3c55cfbda6750b4
AlexGaiser <alexcgaiser@gmail.com>
2019-03-10 01:13:49 -0500

    changes to settings.py and urls.py to include reddit module

--- djangonlp/settings.py
+++ djangonlp/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'start', 
+    'reddit', 
+    'start',
     'rest_framework'
 ]
 
