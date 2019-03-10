c67639795bd9e4075b0d75734f2b14fa4cc943e3
AlexGaiser <alexcgaiser@gmail.com>
2019-03-10 14:06:17 -0400

    removed reddit

--- djangonlp/settings.py
+++ djangonlp/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'reddit', 
+    # 'reddit', 
     'start',
     'rest_framework'
 ]
--- djangonlp/urls.py
+++ djangonlp/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('reddit.urls'))
+    path('', include('start.urls'))
 ]
