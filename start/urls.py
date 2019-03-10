52f236ec3517e7595ce5239e64cddfce2551cb67
AlexGaiser <alexcgaiser@gmail.com>
2019-03-10 14:28:25 -0400

    added page to show environment variables at /db

--- start/urls.py
+++ start/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.LeadListCreate.as_view(), name='index'),
+    path('db', views.dbpage)
 ]
--- start/views.py
+++ start/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 # Create your views here.
-
+import os
 from start.models import Lead
 from start.serializers import LeadSerializer
 from rest_framework import generics
@@ -13,3 +13,28 @@ class LeadListCreate(generics.ListCreateAPIView):
 def startpage(request):
     return HttpResponse("Hello, world.This means that everything is working")
 
+def dbpage(request):
+    if 'RDS_DB_NAME' in os.environ:
+        DATABASES = {
+                'DEPLOYED': 'Deployed',
+                'ENGINE': 'django.db.backends.postgresql_psycopg2',
+                'NAME': os.environ['RDS_DB_NAME'],
+                'USER': os.environ['RDS_USERNAME'],
+                'PASSWORD': os.environ['RDS_PASSWORD'],
+                'HOST': os.environ['RDS_HOSTNAME'],
+                'PORT': os.environ['RDS_PORT'],
+            }
+    else:
+        DATABASES = {
+                'LOCAL': 'local',
+                'ENGINE': 'django.db.backends.postgresql_psycopg2',
+                'NAME': 'django1',
+                'USER': '',
+                'PASSWORD': '',
+                'HOST': '',
+                'PORT': '',
+        }
+        
+    
+    return JsonResponse(DATABASES, safe=False)
+    # return HttpResponse("Hello, world.This means that everything is working")
