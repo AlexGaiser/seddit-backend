8913df3d07217cd1a4a014ed2d85fb1a798890fe
AlexGaiser <alexcgaiser@gmail.com>
2019-03-10 16:14:27 -0400

    rebuilt create_engine in cfg to use instance information , username password, host port dbname

--- reddit/collection_app/cfg.py
+++ reddit/collection_app/cfg.py
@@ -13,15 +13,32 @@ client_id = "CuZZ9Hw2-lENkQ"
 client_secret = "Cu8ruqL1A5LBGHOiGJ32QISpomw"
 password = "A2sSaQq9w4f5E49"
 username = "RavenousDataBot"
-
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+    }
 
 if 'RDS_DB_NAME' in os.environ:
     dbname = os.environ['RDS_DB_NAME']
+    dbuser = os.environ['RDS_USERNAME']
+    dbpassword = os.environ['RDS_PASSWORD']
+    dbhost = os.environ['RDS_HOSTNAME']
+    dbport = os.environ['RDS_PORT']
 
+    db = create_engine('postgresql+psycopg2:///%s:%s@%s/%s:%s' % (dbuser,dbpassword, dbhost, dbport, dbname))
+
+    print(db)
 else:
     dbname = 'django1'
+    db =  create_engine('postgresql+psycopg2:///%s' % (dbname))
 
-db = create_engine('postgresql+psycopg2:///%s' % (dbname))
 
 
 meta = MetaData(bind=db, reflect=True )
